8fb4e200291c41cac8299e376e31cdb2140a4bb1
SaIdEeVaN <saideevan@gmail.com>
2026-04-15 20:14:18 +0530

    Made changes to the C logo

--- src/app/components/SkillsLogoGrid.tsx
+++ src/app/components/SkillsLogoGrid.tsx
@@ -19,7 +19,7 @@ const SKILLS: SkillLogo[] = [
   { name: "HTML", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
   { name: "CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
   { name: "JavaScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-  { name: "C", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg" },
+  { name: "C", src: "/skills/c.png" },
   { name: "C++", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" },
   { name: "Python", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" },
   { name: "TypeScript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" },
